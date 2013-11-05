5185d295bbc152cc9464f23eb560e34f3b84bbc4
jxnblk <jackson@kickstarter.com>
2013-11-05 13:39:45 -0500

    basics

--- js/controllers.js
+++ js/controllers.js
@@ -2,21 +2,16 @@
 
 
 rprtr.controller('ReportCtrl',
-  ['$scope', '$http', '$location', 'declarations', 'declarationsByType', 'selectors', 'createUniques',
-  function($scope, $http, $location, declarations, declarationsByType, selectors, createUniques) {
+  ['$scope', '$http', '$location', '$routeParams', 'declarations', 'declarationsByType', 'selectors', 'createUniques',
+  function($scope, $http, $location, $routeParams, declarations, declarationsByType, selectors, createUniques) {
 
-    console.log('GlobalCtrl');
-
-    // Kinda hacky way of doing the select
-    $scope.sites = { 'value': 'github', 'values': ['github', 'kickstarter', 'mapbox', 'medium', 'myspace', 'twitter', 'salesforce', 'sfdc', 'newyorktimes', 'css', 'bbc', 'bootstrap', 'topcoat'] };
 
     // Setting as a scope variable that can be updated in the view
     if($scope.styleData == null) {
-      $scope.styleData = 'github';
-      $scope.sites.value = 'github';
+      $location.path('/kickstarter');
+      $scope.styleData = 'kickstarter';
       console.log('getting styles for ' + $scope.styleData);
     };
-    console.log($scope.styleData);
 
     // Function to get the styles data - This should really go in a factory
     $scope.getStyles = function(styleData) {
@@ -24,10 +19,7 @@ rprtr.controller('ReportCtrl',
       $http.get('data/' + styleData + '/rules.json').success(function(res) {
         $scope.styles = res;
         selectors($scope);
-
-
       });
-      // This might break the parser
       $http.get('data/' + styleData + '/declarations.json').success(function(res){
         $scope.declarations = res;
         // Create arrays for each declaration type in the factory
@@ -57,82 +49,6 @@ rprtr.controller('ReportCtrl',
     };
 
 }]);
-
-
-rprtr.controller('HomeCtrl', ['$scope', '$filter', '$routeParams', function($scope, $filter, $routeParams) {
-
-  $scope.stylesData = 'kickstarter';
-
-
-  $scope.$watch('loading', function(){
-    console.log('checking for warnings...');
-    if($scope.uniqueDeclarations) $scope.refactoringPotential = parseInt((1 - ($scope.uniqueDeclarations.length / $scope.declarations.length)) * 100);
-    if($scope.fontSizes) {
-      if($scope.fontSizes.length > 128) {
-        $scope.fontSizesWarning = 'You have over 128 font-size declarations, u r silly.';
-      } else if($scope.fontSizes.length > 512) {
-        $scope.fontSizesWarning = 'Over 512 font-size declarations? Go home. You are drunk.';
-      };
-    };
-    if($scope.uniqueFontSizes){
-      if($scope.uniqueFontSizes.length > 64) {
-        $scope.uniqueFontSizesWarning = 'You have over 64 unique font sizes. Type scale much?';
-      } else if ($scope.uniqueFontSizes.length > 128) {
-        $scope.uniqueFontSizesWarning = 'Over 128 unique font sizes. Alright, you\'ve lost your computer privileges.';
-      };
-    };
-    if($scope.declarations){
-      if($scope.declarations.length > 4095) {
-        $scope.declarationsWarning = 'You have ' + $scope.declarations.length + ' selectors. Internet Explorer supports a maximum of 4095 selectors per stylesheet. Also, that is a lot.'
-      };
-    };
-
-  });
-
-
-
-}]);
-
-rprtr.controller('MarginCtrl', ['$scope', 'anythingToRelative', function($scope, anythingToRelative){
-  anythingToRelative($scope.margins);
-}]);
-
-rprtr.controller('PaddingCtrl', ['$scope', 'anythingToRelative', function($scope, anythingToRelative){
-  anythingToRelative($scope.paddings);
-}]);
-
-rprtr.controller('WidthCtrl', ['$scope', 'anythingToRelative', function($scope, anythingToRelative){
-  anythingToRelative($scope.widths);
-}]);
-
-rprtr.controller('HeightCtrl', ['$scope', 'anythingToRelative', function($scope, anythingToRelative){
-  anythingToRelative($scope.heights);
-}]);
-
-rprtr.controller('ColorCtrl', ['$scope', function($scope){
-  $scope.$watch('loading', function(){
-    $scope.viewColors = $scope.colors;
-  });
-  $scope.showAll = function(){
-    $scope.viewColors = $scope.colors;
-  };
-  $scope.showUnique = function() {
-    $scope.viewColors = $scope.uniqueColors;
-  };
-}]);
-
-rprtr.controller('BackgroundColorCtrl', ['$scope', function($scope){
-  $scope.$watch('loading', function(){
-    $scope.viewBackgroundColors = $scope.backgroundColors;
-  });
-  $scope.showAll = function() {
-    $scope.viewBackgroundColors = $scope.backgroundColors;
-  };
-  $scope.showUnique = function() {
-    $scope.viewBackgroundColors = $scope.uniqueBackgroundColors;
-  };
-}]);
-
 
 
 rprtr.controller('ParserCtrl', ['$scope', '$http', '$filter', 'declarations', function($scope, $http, $filter, declarations){
